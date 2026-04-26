43999dd5107e68cb5da5d27e650f3a3b618ff22b
monagreda <monix.lm@gmail.com>
2026-04-26 16:53:58 -0400

    ajustes para render

--- backend/main.py
+++ backend/main.py
@@ -27,6 +27,9 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
 # Middleware
 app.add_middleware(
     CORSMiddleware,
